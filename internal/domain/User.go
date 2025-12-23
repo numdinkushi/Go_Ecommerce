8fa18da6c827b2234ac4060b112e2626f21d8325
Kushi Numdin <numdinkushi@gmail.com>
2025-12-23 15:51:06 +0100

    db migrations

--- internal/domain/User.go
+++ internal/domain/User.go
@@ -3,16 +3,20 @@ package domain
 import "time"
 
 type User struct {
-	ID        uint    `json:"id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Email     string `json:"email"`
-	Phone     string `json:"phone"`
-	Code      int `json:"code"`
+	ID        uint    `json:"id" gorm:"primaryKey"`
+	FirstName string `json:"first_name" gorm:"not null"`
+	LastName  string `json:"last_name" gorm:"not null"`
+	Email     string `json:"email" gorm:"not null;unique"`
+	Phone     string `json:"phone" gorm:"index;not null;unique"`
+	Code      int `json:"code" gorm:"not null"`
 	Expiry    time.Time `json:"expiry"`
-	Verified  bool `json:"verified"`
+	Verified  bool `json:"verified" gorm:"default:false"`
 	Password  string `json:"password"`
-	UserType  string `json:"user_type"`
-	// CreatedAt time.Time `json:"created_at"`
-	// UpdatedAt time.Time `json:"updated_at"`
+	UserType  string `json:"user_type" gorm:"default:buyer"`
+	CreatedAt time.Time `json:"created_at" gorm:"default:CURRENT_TIMESTAMP"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"default:CURRENT_TIMESTAMP"`
 }
+
+// func (User) TableName() string {
+// 	return "users"
+// } 
